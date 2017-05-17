2724d5a7a792a82823fe3449ed0988a580920c5d
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-05-17 10:59:47 -0600

    Style changes

--- marvel.js
+++ marvel.js
@@ -1,40 +1,29 @@
 $(document).ready(function () {
-  // var api = require('http(s)://gateway.marvel.com/')
 
-  // var marvel = api.createClient({
-  //   publicKey: '1beea86e559761cbc8bcee0e0aef1ab6',
-  //   privateKey: ''
-  // $.get("https://gateway.marvel.com:443/v1/public/characters?name=hulk&apikey=1beea86e559761cbc8bcee0e0aef1ab6")
-  //   .then(function (results) {
-  //     console.log(results);
-  //   })
-
+  // *********Engage Submit Button ********************
   $("form").submit(function (event) {
     event.preventDefault();
-    // itirate through all children of select menu, find the one which has the selected attribute
-    // & then get the value of the selected item. Set that to global variable.
+
+    // ****** API Request needs the Character ID & Public API Key ********
+    // ****** Get Character ID when Hero is selected + submit button *******
+
     var characterID = $('select').val();
+
     console.log(characterID);
+
     var apiRequest = "https://gateway.marvel.com:443/v1/public/characters/" + characterID + "/comics?apikey=1beea86e559761cbc8bcee0e0aef1ab6"
 
     $.get(apiRequest)
       .then(function(results) {
+
+        // ******* Itirate through results array for Images to return ********
+
         for(var i =0; i <results.data.results.length; i++){
           console.log(results.data.results[i].images[0].path)
           var html = "<img src='" + results.data.results[i].images[0].path + "/portrait_xlarge.jpg'" + "/>";
           $(".results").append(html);
-        }
-
-        // for(var i=0; i<results.length; i++){
-        // console.log(results.data.results);
+          }
         })
         $(".results").empty();
       })
     })
-
-
-
-  // $("supers").change(function() {
-  //   option = $("supers").val();
-  // });
-//
